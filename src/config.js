128c1fea27ba4a9e61ccd8dba42284a7dacfc9fc
gerhartz <paulgerhartz19@gmail.com>
2021-01-08 09:41:32 -0600

    Signup, Login and CRUD functions set up
    - Added pages for Signup and Login
    - CRUD operations for notes
    - List all notes on the homepage

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const config = {
+    STRIPE_KEY: "pk_test_51I73sXGQsdkSfPfwgataI6yLzMp8nr5lZWpZkJDoZytMKJfBzgYTaYjwJuDDJk5DLiVGULjEo1jtOXcJT7YqlVsl002L3a9RTy",
     s3: {
       REGION: "us-east-1",
       BUCKET: "notes-app-upload-paulg",
@@ -13,6 +14,7 @@ const config = {
       APP_CLIENT_ID: "2orjeovg8ahit7269il36et9h2",
       IDENTITY_POOL_ID: "us-east-1:99d377f8-1568-4281-8f46-49cf90df36a1",
     },
+    MAX_ATTACHMENT_SIZE: 5000000
   };
   
 export default config;
